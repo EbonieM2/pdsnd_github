cf474732f5a425a704cd04e9ca3ee80c2230ef25
Ebonie-M <ebonie.mcsweaney@outlook.com>
2021-02-15 23:10:14 -0500

    refactor: Change input message in city loop
    Change message text from "Okay, I'll show you bikeshare data for:" to
    "Here is bikeshare data for:."

--- bikeshare_2.py
+++ bikeshare_2.py
@@ -27,7 +27,7 @@ def get_filters():
     while city not in cities:
         city = input("Please choose Chicago, New York City or Washington: ").lower()
         if city.casefold() in cities:
-            print("Okay, I'll show you bikeshare data for " + city)
+            print("Here is bikeshare data for " + city)
 
     # TO DO: get user input for month (all, january, february, ... , june)
     months = ["all", "january", "february", "march", "april", "may", "june"]
